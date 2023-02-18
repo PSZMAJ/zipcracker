c2da6f311fcf1956457b92906d993ade1c190db2
Przemysław Szmaj <37410338+PSZMAJ@users.noreply.github.com>
2023-02-18 19:16:48 +0100

    zipcracker v1.0

---
+++ zipcracker.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+
+zainstaluj_wymagania()
+{
+	sudo apt-get update && sudo apt-get upgrade -y
+	sudo apt-get install fcrackzip -y zip -y unzip -y
+	clear && sudo bash zipcracker.sh
+}
+
+Pobierz_slownik()
+{
+	sudo rm top_polskie_hasla.txt
+	sudo wget https://raw.githubusercontent.com/PSZMAJ/WORDLISTS/main/top_polskie_hasla.txt
+	sudo chmod 777 top_polskie_hasla.txt
+	clear && sudo bash zipcracker.sh
+}
+
+fcrackzip_wordlist_moj()
+{
+	echo "Podaj sciezke do pliku np. /home/kali/Desktop/plik.zip";
+	read sciezka
+	echo "Podaj sciezke do slownika np. /usr/share/wordlist/rockyou.txt"
+	read slownik
+	sudo fcrackzip -u -D -p $slownik $sciezka
+		
+}
+
+fcrackzip_wordlist_wbudowany()
+{
+	echo "Podaj sciezke do pliku np. /home/kali/Desktop/plik.zip";
+	read sciezka
+	sudo fcrackzip -u -D -p top_polskie_hasla.txt $sciezka	
+}
+
+zip2john_brute()
+{
+	echo "Podaj sciezke do pliku np. /home/kali/Desktop/plik.zip";
+	read sciezka
+	sudo zip2john $sciezka > hash.txt
+	sudo chmod 777 hash.txt
+	john --wordlist=top_polskie_hasla.txt hash.txt
+	sleep 5
+	sudo rm hash.txt
+}
+
+own_zip_protect()
+{
+	echo "Podaj nazwe paczki np. zabezpieczone.zip";
+	read paczka
+	echo "podaj sciezke do zabezpieczenia pliku ";
+	read sciezka
+	sudo zip -p $paczka $sciezka && sudo zip -e $paczka $sciezka
+	sudo bash zipcracker.sh
+}
+
+exit_work()
+{
+	exit
+}
+
+
+figlet zipcracker 1.0
+echo " ------------------------------------------------------------------------------ "
+echo "   Narzędzie powstało tylko do celów edukacyjnych! --> zipcracker Version 1.0"
+echo " ------------------------------------------------------------------------------ "
+echo -e "\e[5m  KUP KURS NA WWW.EHAKER.PL \e[0m"
+echo -e "\e[5m  KUP KURS NA WWW.EHACKING.PL \e[0m"
+echo "Wybierz opcje"
+echo "================="
+echo -e "\e[31m  [0] ZAINSTALUJ WYMAGANIA \e[0m"
+echo -e "\e[31m  [1] (fcrackzip) SZYBKIE LAMANIE HASLA - SLOWNIK UZYTKOWNIKA\e[0m"
+echo -e "\e[31m  [2] (fcrackzip) SZYBKIE LAMANIE HASLA - SLOWNIK WBUDOWANY\e[0m"
+echo -e "\e[31m  [3] (john2zip) SZYBKIE LAMANIE HASLA - SLOWNIK WBUDOWANY\e[0m"
+
+echo -e "\e[31m  [99] Zabezpiecz pliki - Generowanie zipa z haslem\e[0m"
+echo -e "\e[31m  [100] Pobierz najnowszy slownik hasel \e[0m"
+echo -e "\e[31m  [101] WYJSCIE \e[0m"
+
+
+read opcja
+ 
+
+case "$opcja" in
+
+  "0") zainstaluj_wymagania ;;
+  "1") fcrackzip_wordlist_moj ;;
+  "2") fcrackzip_wordlist_wbudowany ;;
+  "3") zip2john_brute ;;
+  "99") own_zip_protect ;;
+  "100") Pobierz_slownik ;;
+  
+  "101") exit_work ;;
+  *)  clear && ./zipcracker.sh
+  
+esac
+
+
+
+exit
