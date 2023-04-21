bb0716a6a5bb8d390a998fe3b02ad3bd1cc7c073
Przemysław Szmaj <37410338+PSZMAJ@users.noreply.github.com>
2023-04-21 16:22:30 +0200

    Update zipcracker.sh

--- zipcracker.sh
+++ zipcracker.sh
@@ -4,6 +4,10 @@
 zainstaluj_wymagania()
 {
 	sudo apt-get update && sudo apt-get upgrade -y
+	git clone https://github.com/hamedA2/Zydra.git
+	cd Zydra && sudo chmod 777 *
+	sudo bash script.sh
+	cd ..
 	sudo apt-get install fcrackzip -y zip -y unzip -y
 	clear && sudo bash zipcracker.sh
 }
@@ -13,6 +17,10 @@ Pobierz_slownik()
 	sudo rm top_polskie_hasla.txt
 	sudo wget https://raw.githubusercontent.com/PSZMAJ/WORDLISTS/main/top_polskie_hasla.txt
 	sudo chmod 777 top_polskie_hasla.txt
+	cd Zydra && rm top_polskie_hasla.txt
+	sudo wget https://raw.githubusercontent.com/PSZMAJ/WORDLISTS/main/top_polskie_hasla.txt
+	sudo chmod 777 top_polskie_hasla.txt
+	cd ..
 	clear && sudo bash zipcracker.sh
 }
 
@@ -44,6 +52,16 @@ zip2john_brute()
 	sudo rm hash.txt
 }
 
+zydra_wordlists()
+{
+	echo "Podaj sciezke do pliku np. /home/kali/Desktop/plik.zip";
+	read sciezka
+	cd Zydra
+	python3 Zydra.py -f $sciezka -d top_polskie_hasla.txt
+	cd .. && bash zipcracker.sh
+
+}
+
 own_zip_protect()
 {
 	echo "Podaj nazwe paczki np. zabezpieczone.zip";
@@ -60,9 +78,9 @@ exit_work()
 }
 
 
-figlet zipcracker 1.0
+figlet zipcracker 1.1
 echo " ------------------------------------------------------------------------------ "
-echo "   Narzędzie powstało tylko do celów edukacyjnych! --> zipcracker Version 1.0"
+echo "   Narzędzie powstało tylko do celów edukacyjnych! --> zipcracker Version 1.1"
 echo " ------------------------------------------------------------------------------ "
 echo -e "\e[5m  KUP KURS NA WWW.EHAKER.PL \e[0m"
 echo -e "\e[5m  KUP KURS NA WWW.EHACKING.PL \e[0m"
@@ -71,7 +89,8 @@ echo "================="
 echo -e "\e[31m  [0] ZAINSTALUJ WYMAGANIA \e[0m"
 echo -e "\e[31m  [1] (fcrackzip) SZYBKIE LAMANIE HASLA - SLOWNIK UZYTKOWNIKA\e[0m"
 echo -e "\e[31m  [2] (fcrackzip) SZYBKIE LAMANIE HASLA - SLOWNIK WBUDOWANY\e[0m"
-echo -e "\e[31m  [3] (john2zip) SZYBKIE LAMANIE HASLA - SLOWNIK WBUDOWANY\e[0m"
+echo -e "\e[31m  [3] (john2zip) SZYBKIE LAMANIE HASLA - SLOWNIK WBUDOWANY\e[0m - NIEAKTYWNY, oczekuj aktualizacji!"
+echo -e "\e[31m  [4] (zydra) SZYBKIE LAMANIE HASLA - SLOWNIK WBUDOWANY\e[0m - NOWOSC" 
 
 echo -e "\e[31m  [99] Zabezpiecz pliki - Generowanie zipa z haslem\e[0m"
 echo -e "\e[31m  [100] Pobierz najnowszy slownik hasel \e[0m"
@@ -87,6 +106,7 @@ case "$opcja" in
   "1") fcrackzip_wordlist_moj ;;
   "2") fcrackzip_wordlist_wbudowany ;;
   "3") zip2john_brute ;;
+  "4") zydra_wordlists ;;
   "99") own_zip_protect ;;
   "100") Pobierz_slownik ;;
   
